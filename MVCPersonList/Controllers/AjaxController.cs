de06973aa4adb2252995d05efd3806d0c76d9e87
Lars Brevén <lars.breven@telia.com>
2021-04-29 16:10:39 +0200

    Ajax-Controller added, included view redirect.

---
+++ MVCPersonList/Controllers/AjaxController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPersonList.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExampleOne(string messageText)             // Get a form from the Index-page that trigger ExampleOne
+        {
+            ViewBag.Msg = messageText;                                  // Gets the dynamic ViewBag with the text in the string "messageText"
+            return View("Index");                                       // The view will navigate to Index
+        }
+
+        public IActionResult ExampleTwo(string messageText)             // Get a form from the Index-page that trigger ExampleTwo
+        {
+            ViewBag.Msg = messageText;                                  // Gets the dynamic ViewBag with the text in the string "messageText"
+            return RedirectToAction("Index");                           // The "messagetext" will be redirected to Index and reload the full page
+        }
+    }
+}
--- MVCPersonList/Controllers/HomeController.cs
+++ MVCPersonList/Controllers/HomeController.cs
@@ -6,21 +6,33 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MVCPersonList.Models;
+using MVCPersonList.Models.Data;
+using MVCPersonList.Models.Service;
 
 namespace MVCPersonList.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;               // Diagnostic tool
+        private readonly IPeopleService _peopleService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            _peopleService = new PeopleService();
         }
 
+       
         public IActionResult Index()                                    // Index page
         {
-            return View();
+                                                                        // Only for demonstation purpose
+            List<Person> personList = _peopleService.All().PersonList;  // Only the personlist is needed
+            Person lastPerson = null;                                   // Null by default
+            if(personList.Count > 0)                                    // Must be one person in the list
+            {
+                lastPerson = personList[personList.Count - 1];          // The last (newest) person in the list
+            }                                                         
+                                                                        // End of demonstration purpose
+
+            return View(lastPerson);                                    // Last person is returned to Home Index
         }
 
         public IActionResult Privacy()
