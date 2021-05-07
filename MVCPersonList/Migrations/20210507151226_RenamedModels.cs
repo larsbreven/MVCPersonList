866716d684ed57912c6341c48f807b96ce12146c
Lars Brevén <lars.breven@telia.com>
2021-05-07 17:14:16 +0200

    Renamed Models

---
+++ MVCPersonList/Migrations/20210507151226_RenamedModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCPersonList.Migrations
+{
+    public partial class RenamedModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
