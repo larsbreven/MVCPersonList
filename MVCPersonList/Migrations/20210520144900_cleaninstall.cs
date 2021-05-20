b747093574aa46e0949056bb716a0a6bce501ae6
Lars Brevén <lars.breven@telia.com>
2021-05-20 16:54:11 +0200

    Database updated.

---
+++ MVCPersonList/Migrations/20210520144900_cleaninstall.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCPersonList.Migrations
+{
+    public partial class cleaninstall : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "People");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InCityId",
+                table: "People",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityName",
+                table: "Cities",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Cities",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonLanguages",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    LanguageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonLanguages", x => new { x.PersonId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_InCityId",
+                table: "People",
+                column: "InCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonLanguages_LanguageId",
+                table: "PersonLanguages",
+                column: "LanguageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_People_Cities_InCityId",
+                table: "People",
+                column: "InCityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_People_Cities_InCityId",
+                table: "People");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "PersonGroups");
+
+            migrationBuilder.DropTable(
+                name: "PersonLanguages");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_People_InCityId",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "InCityId",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Cities");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "People",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityName",
+                table: "Cities",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+        }
+    }
+}
